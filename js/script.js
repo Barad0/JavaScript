9217152851b0f398ea63eeb02db92d4ae361bf96
Roman Stepanov <9107794@gmail.com>
2016-11-26 16:42:39 +0700

    v5

---
+++ js/script.js
@@ -0,0 +1,115 @@
+$(function () { 
+    
+    $('#createUserSubmit').click( sendData );
+    $('#getUsers').click( getData );
+
+    
+    function getData () {
+       
+        $.ajax({
+            type: 'GET',
+            url: 'http://92.53.104.115:3000/api/v1/users',
+            
+            beforeSend: function ( jqXHR, settings ) {
+              
+                console.log('Я выполняюсь перед отправкой ajax-запроса!');
+                // return false; //если в этом методе вернуть false, то ajax-запрос не выполнится
+            },
+            error: function ( jqXHR, textStatus, errorThrown ) {
+              
+                console.log('Я выполняюсь в случае ошибки!');
+                // console.log('Время превышено');
+            },
+            success: function ( data, textStatus, errorThrown ){
+            var usersList = $('#userList');
+                data.forEach(function (user) {
+                    usersList.append('<li class="list-group-item">' + user.name + '</li>')
+                })
+                console.log(data);
+            },
+            complete: function ( jqXHR, textStatus ) {
+               
+                console.log('Мне всё равно с каким статусом завершился ajax-запрос, я выполнюсь в любом случае!');
+            }
+        });
+
+        return false;
+    }
+
+    function sendData () {
+        var data         = {};
+            data.name    = $('#inputName').val();
+            data.surname = $('#inputSurname').val();
+            data.lasname = $('#inputLastName').val();
+            data.email   = $('#inputEmail').val();
+
+        $.ajax({
+            type: 'POST',
+            url: 'http://92.53.104.115:3000/api/v1/users',
+            data: data,
+            dataType: 'json', //script, html, json, jsonp
+            beforeSend: function ( jqXHR, settings ) {
+                $('#createUserSubmit').prop('disabled', true)
+                $('#createUserSubmit span').css('opacity', 0.5);
+                $('#createUserSubmit .cssload-container').css('display', 'block');
+                console.log(jqXHR);
+                console.log(settings);
+                console.log('Я выполняюсь перед отправкой ajax-запроса!');
+                // return false; //если в этом методе вернуть false, то ajax-запрос не выполнится
+            },
+            error: function ( jqXHR, textStatus, errorThrown ) {
+                console.log(jqXHR);
+                console.log(textStatus);
+                console.log(errorThrown);
+                console.log('Я выполняюсь в случае ошибки!');
+                // console.log('Время превышено');
+            },
+            success: function ( data, textStatus, errorThrown ){
+                $(".success_w").show();
+                $('#inputName').val('');
+                $('#inputSurname').val('');
+                $('#inputLastName').val('');
+                $('#inputEmail').val('');
+                console.log('Пользователь создан');
+            },
+            complete: function ( jqXHR, textStatus ) {
+                $('#createUserSubmit').prop('disabled', false)
+                $('#createUserSubmit span').css('opacity', 1);
+                $('#createUserSubmit .cssload-container').css('display', 'none');
+                console.log(jqXHR);
+                console.log(textStatus);
+                console.log('Мне всё равно с каким статусом завершился ajax-запрос, я выполнюсь в любом случае!');
+            }
+        });
+
+        return false;
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
